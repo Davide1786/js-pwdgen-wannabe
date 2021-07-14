dc95bb0dbe5b574e54da8028bb909f5d923e5bb1
Davide <dadavidenapolitano@gmail.com>
2021-07-14 16:33:24 +0200

    Esercizio base piu primo bonus

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21.
+
+/*
+Esercizio base
+*/
+
+var il_tuo_nome = (prompt('Inserisci il tuo nome'));
+
+var il_tuo_cognome = (prompt('Inserisci il tuo cognome'));
+
+var il_tuo_colore_preferito = (prompt('Inserisci il tuo colore preferito'));
+
+
+document.getElementById('nome').innerHTML =('Il tuo nome è ' + il_tuo_nome);
+
+/*
+Esercizio bonus righe
+*/
+
+var il_tuo_nome = (prompt('Inserisci il tuo nome'));
+
+var il_tuo_cognome = (prompt('Inserisci il tuo cognome'));
+
+var il_tuo_colore_preferito = (prompt('Inserisci il tuo colore preferito'));
+
+
+document.getElementById('nome').innerHTML =('Il tuo nome è ' + il_tuo_nome);
+document.getElementById('cognome').innerHTML =('Il tuo cognome è ' + il_tuo_cognome);
+document.getElementById('colore').innerHTML =('Il tuo colore preferito è ' + il_tuo_colore_preferito);
+
+
