934acd24f7f79d533d2752dbc28f042d6d417981
Davide <dadavidenapolitano@gmail.com>
2021-07-14 19:05:45 +0200

    Super bonus

--- js/main.js
+++ js/main.js
@@ -4,28 +4,46 @@
 Esercizio base
 */
 
-var il_tuo_nome = (prompt('Inserisci il tuo nome'));
+// var il_tuo_nome = (prompt('Inserisci il tuo nome'));
 
-var il_tuo_cognome = (prompt('Inserisci il tuo cognome'));
+// var il_tuo_cognome = (prompt('Inserisci il tuo cognome'));
 
-var il_tuo_colore_preferito = (prompt('Inserisci il tuo colore preferito'));
+// var il_tuo_colore_preferito = (prompt('Inserisci il tuo colore preferito'));
 
 
-document.getElementById('nome').innerHTML =('Il tuo nome è ' + il_tuo_nome);
+// document.getElementById('nome').innerHTML =('Il tuo nome è ' + il_tuo_nome);
 
 /*
 Esercizio bonus righe
 */
 
-var il_tuo_nome = (prompt('Inserisci il tuo nome'));
+// var il_tuo_nome = (prompt('Inserisci il tuo nome'));
 
-var il_tuo_cognome = (prompt('Inserisci il tuo cognome'));
+// var il_tuo_cognome = (prompt('Inserisci il tuo cognome'));
 
-var il_tuo_colore_preferito = (prompt('Inserisci il tuo colore preferito'));
+// var il_tuo_colore_preferito = (prompt('Inserisci il tuo colore preferito'));
 
 
-document.getElementById('nome').innerHTML =('Il tuo nome è ' + il_tuo_nome);
-document.getElementById('cognome').innerHTML =('Il tuo cognome è ' + il_tuo_cognome);
-document.getElementById('colore').innerHTML =('Il tuo colore preferito è ' + il_tuo_colore_preferito);
+// document.getElementById('nome').innerHTML =('Il tuo nome è ' + il_tuo_nome);
+// document.getElementById('cognome').innerHTML =('Il tuo cognome è ' + il_tuo_cognome);
+// document.getElementById('colore').innerHTML =('Il tuo colore preferito è ' + il_tuo_colore_preferito);
+
+/*
+Esercizio bonus righe + css
+*/
+
+var web = (prompt('Scivi Sito o E-commerce'));
+
+var budget = (prompt('Il tuo buget'));
+
+var consegna = (prompt('Tempi di consegna'));
+
+var bonus = (prompt('Richieste particolari'));
+
+
+document.getElementById('scegli').innerHTML =('Hai scelto: ' + web);
+document.getElementById('importo').innerHTML =('Vuoi spendere: ' + budget);
+document.getElementById('deadline').innerHTML =('Il tempo di consegna è: ' + consegna);
+document.getElementById('richiesta').innerHTML =('Il tuo bonus è: ' + bonus);
 
 
